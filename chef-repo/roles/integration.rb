6f83ef5f6b8f9c6f01c8e30a890d364680adddf3
Nik Ormseth <nik@ormseth.net>
2014-08-26 11:09:53 -0700

    changed name of slave recipe in build-master

--- chef-repo/roles/integration.rb
+++ chef-repo/roles/integration.rb
@@ -1,3 +1,3 @@
 name 'integration'
-description 'rails stack in a single machine'
-run_list 'recipe[rails_infrastructure::default]'
+description 'rails stack in a single machine, plus jenkins slave requirements'
+run_list 'recipe[build-master::slave-ruby]','recipe[rails_infrastructure::default]'
